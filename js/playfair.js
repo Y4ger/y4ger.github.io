9653d8a747150afb459f948f69e3261014b39320
Jacob Yager <jacyager@gmail.com>
2018-08-22 12:53:43 -0500

    Cipher Key and Grid Complete

--- js/playfair.js
+++ js/playfair.js
@@ -1,11 +1,28 @@
 /* jshint esversion: 6 */
 
 const cells = document.querySelectorAll('.cell');
-const key = document.querySelector('#key');
+const input = document.querySelector('#key');
 
-const recieveInput = () => {
+const noRepeat = (k) => {
+	var key = '';
+	while (k.length > 0) {
+		var rm = new RegExp(k[0], 'g');
+		key += k[0];
+		k = k.replace(rm, '');
+	}
+	return key;
+};
 
+const setKey = () => {
+	//Remove all whitespace and replace all J with I (by the design of playfair)
+	var rawKey = input.value.toLowerCase().replace(/ +/g, "").replace(/j/g, "i");
+
+	const alph = 'abcdefghiklmnopqrstuvwxyz';
+	rawKey = rawKey.concat(alph);
+	const key = noRepeat(rawKey);
+	var pos = 0;
 	cells.forEach((cell) => {
-		cell.innerHTML = key.value;
+		cell.innerHTML = key[pos];
+		pos++;
 	});
 };
